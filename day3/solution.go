50751d76b2be2f2b65bed943ddf550c3b796ccc2
Martijn Majoor <git@martijnmajoor.nl>
2019-01-14 21:46:33 +0100

    Add solution part 2 for day 3

--- day3/solution.go
+++ day3/solution.go
@@ -8,52 +8,95 @@ import (
 )
 
 type claim struct {
-	id     string
+	id     int
 	x      int
 	y      int
 	width  int
 	height int
 }
 
-func newClaim(input string) *claim {
+func newClaim(input string) claim {
 	pattern := regexp.MustCompile("#(.*) @ (.*),(.*): (.*)x(.*)")
 	matches := pattern.FindStringSubmatch(input)
 
-	id := matches[1]
+	id, _ := strconv.Atoi(matches[1])
 	x, _ := strconv.Atoi(matches[2])
 	y, _ := strconv.Atoi(matches[3])
 	width, _ := strconv.Atoi(matches[4])
 	height, _ := strconv.Atoi(matches[5])
 
-	return &claim{id, x, y, width, height}
+	return claim{id, x, y, width, height}
 }
 
-func squareInchOverlap(lines []string) (overlap int) {
-	claimedAreas := map[int]map[int]int{}
+func (c claim) points() []string {
+	points := []string{}
 
-	for _, line := range lines {
-		claim := newClaim(line)
+	for x := c.x; x < c.x+c.width; x++ {
+		for y := c.y; y < c.y+c.height; y++ {
+			points = append(points, strconv.Itoa(x)+"_"+strconv.Itoa(y))
+		}
+	}
 
-		for x := claim.x; x < claim.x+claim.width; x++ {
-			if claimedAreas[x] == nil {
-				claimedAreas[x] = map[int]int{}
-			}
+	return points
+}
 
-			for y := claim.y; y < claim.y+claim.height; y++ {
-				if claimedAreas[x][y] == 1 {
-					overlap++
-				}
+func squareInchOverlap(claims []claim) (overlap int) {
+	claimed := getClaimedAreas(claims)
 
-				claimedAreas[x][y]++
-			}
+	for _, amount := range claimed {
+		if amount > 1 {
+			overlap++
 		}
 	}
 
 	return
 }
 
+func uniqueClaim(claims []claim) int {
+	claimed := getClaimedAreas(claims)
+
+	for _, claim := range claims {
+		unique := true
+
+		for _, point := range claim.points() {
+			if claimed[point] > 1 {
+				unique = false
+			}
+		}
+
+		if unique {
+			return claim.id
+		}
+	}
+
+	return -1
+}
+
+func getClaimedAreas(claims []claim) map[string]int {
+	claimed := map[string]int{}
+
+	for _, claim := range claims {
+		for _, point := range claim.points() {
+			claimed[point]++
+		}
+	}
+
+	return claimed
+}
+
+func getClaims(lines []string) []claim {
+	claims := []claim{}
+
+	for _, line := range lines {
+		claims = append(claims, newClaim(line))
+	}
+
+	return claims
+}
+
 func main() {
-	claims := file.ReadLines("claims.txt")
+	claims := getClaims(file.ReadLines("claims.txt"))
 
 	fmt.Println("part one:", squareInchOverlap(claims))
+	fmt.Println("part two:", uniqueClaim(claims))
 }
